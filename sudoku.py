3d8b86a56d6c0370faa3f284957997aa3b508491
mishanwong <mishan.wong@gmail.com>
2023-04-24 15:43:25 -0700

    Optimize algorithm

--- sudoku.py
+++ sudoku.py
@@ -2,14 +2,17 @@ import numpy as np
 from collections import Counter
 import copy
 import sys
+import time
 
 
 class SudokuSolver:
     def __init__(self, puzzle):
         self.puzzle = puzzle
         self.solutions = []
+        self.count = 0
 
     def solve(self):
+        self.count += 1
         # If a solution is found, append to the solutions array
         if self.is_solved(self.puzzle):
             self.solutions.append(copy.deepcopy(self.puzzle))
@@ -25,6 +28,25 @@ class SudokuSolver:
                             self.puzzle[row, col] = 0
 
                     return
+
+    def solve_optimized(self):
+        self.count += 1
+        """Alternative solve algorithm that tries to fill in the cell with least degree of freedom"""
+
+        if self.is_solved(self.puzzle):
+            self.solutions.append(copy.deepcopy(self.puzzle))
+
+        cell = self.degress_of_freedom(self.puzzle)
+        if cell:
+            row, col = cell
+            for n in range(1, 10):
+                # Check if it is possible to put this number in this position
+                if self.is_possible(self.puzzle, row, col, n):
+                    self.puzzle[row, col] = n
+                    self.solve_optimized()
+                    self.puzzle[row, col] = 0
+
+            return
 
     def is_solved(self, puzzle):
         """Check if the puzzle is completely solved"""
@@ -106,6 +128,31 @@ class SudokuSolver:
 
         return True
 
+    def degress_of_freedom(self, puzzle):
+        """Returns a list of all empty cells ordered by their degrees of freedom in ascending order"""
+        result = {}
+        for i, row in enumerate(puzzle):
+            for j, num in enumerate(row):
+                if num == 0:
+                    numbers_filled = set()
+                    current_row = puzzle[i]
+                    current_col = puzzle[:, j]
+                    current_grid = []
+                    grid_row = i // 3
+                    grid_col = j // 3
+                    for a in range(grid_row * 3, grid_row * 3 + 3):
+                        for b in range(grid_col * 3, grid_col * 3 + 3):
+                            current_grid.append(puzzle[a][b])
+                    numbers_filled.update(current_row)
+                    numbers_filled.update(current_col)
+                    numbers_filled.update(current_grid)
+                    numbers_filled.remove(0)
+                    deg_freedom = 9 - len(numbers_filled)
+                    result[(i, j)] = deg_freedom
+
+        sorted_result = dict(sorted(result.items(), key=lambda item: item[1]))
+        return list(sorted_result.keys())[0] if sorted_result else None
+
 
 def main():
     # Check usage
@@ -139,7 +186,16 @@ def main():
         sys.exit("Puzzle has invalid input")
 
     solver = SudokuSolver(np.array(puzzle))
-    solver.solve()
+
+    solver.degress_of_freedom(puzzle)
+
+    start_time = time.time()
+    # solver.solve()
+    solver.solve_optimized()
+    end_time = time.time()
+
+    print("Execution time: {:.4f}".format(end_time - start_time))
+    print(f"Number of calls: {solver.count}")
 
     num_solutions = len(solver.solutions)
     if not solver.solutions:
